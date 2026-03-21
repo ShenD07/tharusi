5e90cf5c4382d207b97c31025ee5ff5e7a38915b
ShenD07 <126956165+ShenD07@users.noreply.github.com>
2026-03-21 11:59:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ const CONFIG = {
 
 const photos = [
   {
-    src: "tharusi/img1.png",
+    src: "/img1.png",
     caption: "Replace this with your favorite photo together 💕",
   },
   {
-    src: "img2.png",
+    src: "/img2.png",
     caption: "A memory that still makes you smile ✨",
   },
   {
-    src: "img3.png",
+    src: "/img3.png",
     caption: "Her cutest moment ever 🌸",
   },
 ];
